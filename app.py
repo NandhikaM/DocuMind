8223bf02952d47860534e7200743947e91a48936
Nandhika M <nandhikamurugesan2005@gmail.com>
2026-06-06 12:28:44 +0530

    Implement file upload functionality

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-from flask import Flask, render_template
+import os
+
+from flask import Flask, redirect, render_template, request
 
 app = Flask(__name__)
+UPLOAD_FOLDER = "uploads"
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
 
 @app.route("/")
@@ -8,8 +12,15 @@ def index():
     return render_template("home.html")
 
 
-@app.route("/upload", methods=["GET"])
+@app.route("/upload", methods=["GET", "POST"])
 def upload():
+    if request.method == "POST":
+        # request.files is a dict-like object of uploaded files (FileStorage per form field name)
+        file = request.files["file"]
+        if file.filename:
+            filepath = os.path.join(app.config["UPLOAD_FOLDER"], file.filename)
+            file.save(filepath)
+        return redirect("/")
     return render_template("upload.html")
 
 
