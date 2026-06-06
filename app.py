307d076708b2ef8982433c8d0aa2c64b4b3781ca
Nandhika M <nandhikamurugesan2005@gmail.com>
2026-06-06 12:24:53 +0530

    Document upload form

--- app.py
+++ app.py
@@ -8,5 +8,10 @@ def index():
     return render_template("home.html")
 
 
+@app.route("/upload", methods=["GET"])
+def upload():
+    return render_template("upload.html")
+
+
 if __name__ == "__main__":
     app.run()
